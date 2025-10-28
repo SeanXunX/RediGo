2149e7caf1a7a0244403d12f3e4f91b961cd11c6
sean <seanxunx@outlook.com>
2025-10-28 21:53:21 +0800

    retry

--- app/server/server.go
+++ app/server/server.go
@@ -199,7 +199,7 @@ func (s *Server) SendHandShake() {
 		}
 	}
 	// 完成后再启动 handler 处理后续命令
-	time.Sleep(10 * time.Millisecond) // 如果这样能 100% 成功，说明是竞态问题
+	time.Sleep(100 * time.Millisecond) // 如果这样能 100% 成功，说明是竞态问题
 	h := NewConnHandler(conn, s)
 	go h.Handle(true)
 }
