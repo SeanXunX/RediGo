400086082915a82d744fc0b247e8e9272474fe2d
sean <seanxunx@outlook.com>
2025-10-28 21:52:47 +0800

    test: read rdb

--- app/server/server.go
+++ app/server/server.go
@@ -12,6 +12,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/app/kv"
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
@@ -198,8 +199,9 @@ func (s *Server) SendHandShake() {
 		}
 	}
 	// 完成后再启动 handler 处理后续命令
+	time.Sleep(10 * time.Millisecond) // 如果这样能 100% 成功，说明是竞态问题
 	h := NewConnHandler(conn, s)
-	h.Handle(true)
+	go h.Handle(true)
 }
 
 //	func (s *Server) ReceiveRDB(reader *bufio.Reader) {
